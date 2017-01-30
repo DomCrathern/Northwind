ed22972a4cf924d9152574b314cabd6b1a097b31
Dom <Dom@PC>
2017-01-30 12:57:12 +0000

    Add project files.

---
+++ Northwind/Controllers/ProductController.cs
@@ -0,0 +1,161 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Northwind.Models;
+using PagedList;
+
+namespace Northwind.Controllers
+{
+    public class ProductController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: Product
+        public ActionResult Index(string sortOrder, string currentSearchTerm, string searchTerm, int? pageNumber)
+        {
+            ViewBag.CurrentSortOrder = sortOrder;
+            ViewBag.SortOrder = string.IsNullOrEmpty(sortOrder) ? "desc" : "";
+
+            if (searchTerm == null)
+            {
+                searchTerm = currentSearchTerm;
+            }
+            else
+            {
+                pageNumber = 1;
+            }
+            ViewBag.CurrentSearchTerm = searchTerm;
+
+            var products = db.Products.Include(p => p.Category).Include(p => p.Supplier);
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                products = products.Where(p => p.ProductName.StartsWith(searchTerm));
+            }
+
+            if (sortOrder == "desc")
+            {
+                products = products.OrderByDescending(p => p.ProductName);
+            }
+            else // asc
+            {
+                products = products.OrderBy(p => p.ProductName);
+            }
+
+            return View(products.ToPagedList(pageNumber ?? 1, 15));
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName");
+            return View();
+        }
+
+        // POST: Product/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", product.SupplierID);
+            return View(product);
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", product.SupplierID);
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", product.SupplierID);
+            return View(product);
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Northwind/Controllers/SupplierController.cs
@@ -0,0 +1,153 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Northwind.Models;
+using PagedList;
+
+namespace Northwind.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: Supplier
+        public ActionResult Index(string sortOrder, string currentSearchTerm, string searchTerm, int? pageNumber)
+        {
+            ViewBag.CurrentSortOrder = sortOrder;
+            ViewBag.SortOrder = string.IsNullOrEmpty(sortOrder) ? "desc" : "";
+
+            if (searchTerm == null)
+            {
+                searchTerm = currentSearchTerm;
+            }
+            else
+            {
+                pageNumber = 1;
+            }
+            ViewBag.CurrentSearchTerm = searchTerm;
+
+            var suppliers = db.Suppliers.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                suppliers = suppliers.Where(s => s.CompanyName.StartsWith(searchTerm));
+            }
+
+            if (sortOrder == "desc")
+            {
+                suppliers = suppliers.OrderByDescending(s => s.CompanyName);
+            }
+            else // asc
+            {
+                suppliers = suppliers.OrderBy(s => s.CompanyName);
+            }
+
+            return View(suppliers.ToPagedList(pageNumber ?? 1, 15));
+        }
+
+        // GET: Supplier/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supplier supplier = db.Suppliers.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
+
+        // GET: Supplier/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Supplier/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SupplierID,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax,HomePage")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Suppliers.Add(supplier);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(supplier);
+        }
+
+        // GET: Supplier/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supplier supplier = db.Suppliers.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
+
+        // POST: Supplier/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SupplierID,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax,HomePage")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(supplier).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(supplier);
+        }
+
+        // GET: Supplier/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supplier supplier = db.Suppliers.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
+
+        // POST: Supplier/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Supplier supplier = db.Suppliers.Find(id);
+            db.Suppliers.Remove(supplier);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
